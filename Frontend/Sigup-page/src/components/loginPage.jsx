af988e9fcf2312ce8653a3c4ccdb06aaf48aa5c3
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2024-05-01 19:47:44 +0200

    added tailwind for frontend

---
+++ Frontend/Sigup-page/src/components/loginPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function LoginPage() {
+  <div>
+    <input type="text" placeholder="Username" required />
+    <input type="password" placeholder="password" required />
+    <button type="submit">Submit</button>
+  </div>;
+}

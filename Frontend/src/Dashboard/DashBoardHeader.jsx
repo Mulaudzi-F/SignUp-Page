98dc8edf7119ad9b623cd705d2b757bedc85c711
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-19 14:07:38 +0200

    adjusted the stylings in the header of the dashboard

--- Frontend/src/Dashboard/DashBoardHeader.jsx
+++ Frontend/src/Dashboard/DashBoardHeader.jsx
@@ -1,19 +1,29 @@
+import { useUser } from "../authentication/useUser";
+
 export default function DashBoardHeader() {
+  const { user } = useUser();
+  console.log(user);
   return (
-    <header className="grid grid-cols-3 bg-[#222725] rounded-sm  text-[white] items-center justify-around h-1/6 w-full">
-      <figure className="justify-self-start flex flex-col h-full pl-10  rounded-full">
+    <header className="grid grid-cols-3 shadow-md fixed z-10 bg-[#222725] rounded-sm  text-[white] items-center justify-around w-full">
+      <figure className="justify-self-start flex flex-col h-full pl-6  rounded-full">
         <span className="bg-white w-1/2 flex justify-center items-center h-full rounded-full ">
           {" "}
           <img alt="userPicture" src="" />
         </span>
-        <figcaption>User Name</figcaption>
+        <figcaption className="cursor-pointer hover:text-[#1a759f] pl-2">
+          me
+        </figcaption>
       </figure>
-      <div className="justify-self-center flex items-center h-full w-1/2">
-        <input type="text" className="w-11/12 h-1/2" placeholder="Search" />
+      <div className="justify-self-center flex  items-center h-full w-1/2">
+        <input
+          type="text"
+          className="w-11/12 shadow-2xl rounded-lg  shadow-[black] h-1/2"
+          placeholder="Search"
+        />
       </div>
-      <div className="justify-self-end pr-10 ">
+      <div className="justify-self-end cursor-pointer hover:bg-[#1a759f] pr-10 shadow-inner w-1/4 h-1/2 flex justify-center rounded-lg shadow-[#f8f9fa] ">
         <button>
-          <span>R</span> <span>0.00</span>
+          <span>R</span> <span>{user.balance}</span>
         </button>
       </div>
     </header>

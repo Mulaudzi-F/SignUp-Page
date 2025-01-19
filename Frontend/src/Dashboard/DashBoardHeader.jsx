86a8b33c9f03876c42c4d8884237f58d2cf2abcf
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-19 23:50:59 +0200

    styled the header for mobile device

--- Frontend/src/Dashboard/DashBoardHeader.jsx
+++ Frontend/src/Dashboard/DashBoardHeader.jsx
@@ -1,29 +1,34 @@
 import { useUser } from "../authentication/useUser";
-
+import { FiAlignJustify } from "react-icons/fi";
+const baseUrl = "http://127.0.0.1:8000/";
 export default function DashBoardHeader() {
   const { user } = useUser();
   console.log(user);
   return (
-    <header className="grid grid-cols-3 shadow-md fixed z-10 bg-[#222725] rounded-sm  text-[white] items-center justify-around w-full">
-      <figure className="justify-self-start flex flex-col h-full pl-6  rounded-full">
+    <header className="flex py-4  shadow-md fixed z-10 bg-[#222725] rounded-sm  text-[white] items-center justify-around w-full">
+      <figure className="justify-self-start flex flex-col h-1/2 pl-6 w-1/4 rounded-full">
         <span className="bg-white w-1/2 flex justify-center items-center h-full rounded-full ">
           {" "}
-          <img alt="userPicture" src="" />
+          <img
+            className="rounded-full max-h-24"
+            alt="userPicture"
+            src={`${baseUrl}${user.photo}`}
+          />
         </span>
-        <figcaption className="cursor-pointer hover:text-[#1a759f] pl-2">
+        <figcaption className="cursor-pointer hidden sm:block hover:text-[#1a759f] pl-2">
           me
         </figcaption>
       </figure>
       <div className="justify-self-center flex  items-center h-full w-1/2">
         <input
           type="text"
-          className="w-11/12 shadow-2xl rounded-lg  shadow-[black] h-1/2"
+          className="sm:w-11/12 mr-3 w-full shadow-2xl rounded-lg  shadow-[black] py-2 sm:h-1/2"
           placeholder="Search"
         />
       </div>
-      <div className="justify-self-end cursor-pointer hover:bg-[#1a759f] pr-10 shadow-inner w-1/4 h-1/2 flex justify-center rounded-lg shadow-[#f8f9fa] ">
-        <button>
-          <span>R</span> <span>{user.balance}</span>
+      <div className="justify-self-end cursor-pointer hover:bg-[#1a759f] sm:pr-10 shadow-inner w-1/4 h-1/2 flex justify-center rounded-lg shadow-[#f8f9fa] ">
+        <button className="w-full py-2 ">
+          <span>R</span> <span className="w-full ">{user.balance}</span>
         </button>
       </div>
     </header>

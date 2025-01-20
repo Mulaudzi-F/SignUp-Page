55050821ed55168b76a639c19716a212665fa77f
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-21 00:09:24 +0200

    added the query  for users tutor

--- Frontend/src/service/apiUsers.js
+++ Frontend/src/service/apiUsers.js
@@ -2,10 +2,10 @@ export async function getAllTutorUsers() {
   try {
     const allUsers = await axios.get(`/app/v1/users/getusers`);
     const allUsersData = allUsers.data;
-    let tutor;
-    if (allUsersData.role === "tutor") tutor = allUsersData;
+    let tutors;
+    if (allUsersData.role === "tutor") tutors = allUsersData;
 
-    return tutor;
+    return tutors;
   } catch (error) {
     throw new Error(error.message);
   }

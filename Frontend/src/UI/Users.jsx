cdac8fe271953c7c86d2c1b302795b36bba297fa
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-20 23:16:44 +0200

    completed giving the placeholder for the head of user card

--- Frontend/src/UI/Users.jsx
+++ Frontend/src/UI/Users.jsx
@@ -1,7 +1,20 @@
+import { FaPlus } from "react-icons/fa6";
+
 const UserCard = () => {
   return (
     <section className="bg-red max-w-32">
-      <div></div>
+      <div>
+        <div>
+          <figure>
+            <img alt="user-picture" />
+            <figcaption>Lorem Ipsum</figcaption>
+          </figure>
+          <span>
+            <FaPlus />
+          </span>
+        </div>
+        <p>Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+      </div>
       <div></div>
       <div></div>
       <div></div>

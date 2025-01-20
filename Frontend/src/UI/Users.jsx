52f2873810a12de99ca829c215bd80f5c2d9d4c0
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-20 23:05:54 +0200

    created user card component

---
+++ Frontend/src/UI/Users.jsx
@@ -0,0 +1,10 @@
+const UserCard = () => {
+  return (
+    <section className="bg-red max-w-32">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </section>
+  );
+};

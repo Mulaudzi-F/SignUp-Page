8617011fe474d54e3196ffeba2965d432de7a199
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-21 23:21:41 +0200

    made the dashboard align well with the content

--- Frontend/src/Dashboard/DashBoardLayout.jsx
+++ Frontend/src/Dashboard/DashBoardLayout.jsx
@@ -1,3 +1,5 @@
+import UserCard from "../Features/UsersTutorCard";
+import UserTutorCards from "../UI/TutorCards";
 import DashBoardFooter from "./DashboardFooter";
 import DashBoardHeader from "./DashBoardHeader";
 import DashBoardSideNav from "./DashBoardSideNav";
@@ -5,9 +7,10 @@ import DashBoardSideNav from "./DashBoardSideNav";
 export default function DashboardLayout() {
   return (
     <>
-      <section className="h-screen gap-[34rem] w-full flex flex-col sm:block overflow-x-hidden">
+      <section className="h-screen w-full flex flex-col sm:block overflow-x-hidden">
         <DashBoardHeader />
         <DashBoardSideNav />
+        <UserTutorCards />
         <DashBoardFooter />
       </section>
     </>

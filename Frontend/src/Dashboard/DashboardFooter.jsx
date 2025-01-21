9f63a12478c4c54f75bd8d5bd1b6ff43e8b57cf4
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-21 23:20:03 +0200

    adjeusted the footer to stay fixed on mobile devices

--- Frontend/src/Dashboard/DashboardFooter.jsx
+++ Frontend/src/Dashboard/DashboardFooter.jsx
@@ -2,7 +2,7 @@ import DashBoardNav from "../UI/DashBoardNav";
 
 const DashBoardFooter = () => {
   return (
-    <footer className=" sm:hidden">
+    <footer className=" sm:hidden fixed bottom-0 left-0 right-0 z-50">
       <DashBoardNav />
     </footer>
   );

ef73031328792c75b3c1b06273d455e245670648
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-22 22:23:19 +0200

    made styling adjustment for all  media

--- Frontend/src/Features/UsersTutorCard.jsx
+++ Frontend/src/Features/UsersTutorCard.jsx
@@ -4,35 +4,40 @@ import { IoStarHalf } from "react-icons/io5";
 import { useTutors } from "../../FetchingUsers/useTutors";
 
 const UserCard = ({ onTutorData }) => {
+  const baseUrl = "http://127.0.0.1:8000/";
   console.log(onTutorData);
   return (
-    <section className="bg-red max-w-32">
+    <section className="bg-red shadow-lg  flex flex-col flex-1  w-full rounded-md ">
       <div>
-        <div>
+        <div className="flex justify-between pt-2">
           <figure>
-            <img alt="user-picture" src={onTutorData.photo} />
-            <figcaption>{onTutorData.name}</figcaption>
+            <img
+              alt="user-picture"
+              src={`${baseUrl}${onTutorData.photo}`}
+              className="rounded-r-full max-h-[100px]"
+            />
+            <figcaption className="font-bold pl-2">
+              {onTutorData.name}
+            </figcaption>
           </figure>
-          <span>
-            <FaUserPlus />
+          <span className="m-1">
+            <FaUserPlus size={26} />
           </span>
         </div>
-        <p>title will go here</p>
+        <p className="pl-2">title will go here</p>
       </div>
-      <div>
+      <div className="flex justify-center gap-2">
         {" "}
-        <span>Lorem Ipsum</span> <span> Lorem Ipsum</span>{" "}
-        <span> Lorem Ipsum</span>
+        <span>course 1</span> <span> course 2</span> <span> course 3</span>
       </div>
-      <div>
-        <p>
-          Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
-          Lorem Lorem{" "}
-        </p>
+      <div className="flex justify-center">
+        <p className="text-center">{onTutorData.summary}</p>
       </div>
-      <div>
-        <IoStarHalf />
-        <button>Request</button>
+      <div className="flex justify-between p-8 items-center">
+        <IoStarHalf size={22} />
+        <button className="bg-[#415a77] hover:bg-[#6c757d] text-stone-50 rounded-md p-3">
+          Request
+        </button>
         <p>
           <span>6</span>sesions
         </p>

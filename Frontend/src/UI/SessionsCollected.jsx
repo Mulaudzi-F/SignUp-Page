5df534eaf79dd54b76436b58a5073596697b9010
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-01-27 01:00:36 +0200

    added unorderd list of sessions

---
+++ Frontend/src/UI/SessionsCollected.jsx
@@ -0,0 +1,13 @@
+import SessionCard from "../Features/SessionCard";
+
+const SessionsCollected = () => {
+  return (
+    <>
+      <ul>
+        <SessionCard />
+      </ul>
+    </>
+  );
+};
+
+export default SessionsCollected;

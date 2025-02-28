22f5f56eb26b6e0d22cec172bfe29ca7235b437a
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-28 13:57:49 +0200

    gave the conditional render for listing the session or video call

--- Frontend/src/UI/SessionsCollected.jsx
+++ Frontend/src/UI/SessionsCollected.jsx
@@ -1,11 +1,23 @@
 import SessionCard from "../Features/SessionCard";
+import React, { useContext } from "react";
+import { VideoCallContext } from "../Features/VideoCallContext";
+import VideoCall from "../Features/VideoCall";
 
 const SessionsCollected = () => {
+  const { inCall } = useContext(VideoCallContext);
+
+  const roomUrl = "https://catchup.daily.co/CatchUp";
   return (
     <>
-      <ul className="pt-16">
-        <SessionCard />
-      </ul>
+      {inCall ? (
+        <VideoCall roomUrl={roomUrl} />
+      ) : (
+        <section className="pt-10 text-[#fff]">
+          <ul className="pt-14 flex flex-col items-center gap-4 justify-center">
+            <SessionCard />
+          </ul>
+        </section>
+      )}
     </>
   );
 };

eb0298af7a793195cc732df5d0113917359d331f
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-02-28 13:36:14 +0200

    Replace the profile picture in the header with the app Icon

--- Frontend/src/Dashboard/DashBoardHeader.jsx
+++ Frontend/src/Dashboard/DashBoardHeader.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import { useUser } from "../authentication/useUser";
 import { FiAlignJustify } from "react-icons/fi";
+
 const baseUrl = "http://127.0.0.1:8000/";
 export default function DashBoardHeader() {
   const { user } = useUser();
@@ -7,16 +9,17 @@ export default function DashBoardHeader() {
   return (
     <header className="flex items-center justify-between py-4 fixed top-0 left-0 right-0 z-50 bg-[#222725] text-white shadow-md rounded-sm px-4 sm:px-8">
       {/* Logo Section */}
-      <figure className="flex items-center gap-4">
-        <span className="flex justify-center items-center h-14 w-14 rounded-full overflow-hidden">
-          <img
-            className="h-full w-full object-cover"
-            alt="userPicture"
-            src={`${baseUrl}${user.photo}`}
-          />
-        </span>
-      </figure>
-
+      <Link to={"."}>
+        <figure className="flex items-center gap-4">
+          <span className="flex justify-center items-center h-14 w-14 rounded-full overflow-hidden">
+            <img
+              className="h-full w-full object-cover"
+              alt="Catch up Logo"
+              src="./Catch up logo.webp"
+            />
+          </span>
+        </figure>
+      </Link>
       {/* Search Bar */}
       <div className="flex-grow mx-4 sm:mx-6">
         <input

8826b813bf699e0d611b5842d13665b1aea61208
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-03-03 13:54:40 +0200

    Imported the folder for the videosFeature

---
+++ Frontend/src/Daily Video/views/StartButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**
+ * Props:
+ * - disabled: boolean
+ * - onClick: () => ()
+ */
+export default function StartButton(props) {
+  return (
+    <button
+      className="start-button text-white text-sm font-medium px-4 py-2 rounded-lg hover:bg-blue-700 transition duration-300"
+      disabled={props.disabled}
+      onClick={props.onClick}
+    >
+      ❤️ Connect with Family
+    </button>
+  );
+}
